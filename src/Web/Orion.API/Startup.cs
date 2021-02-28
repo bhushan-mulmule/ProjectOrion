fcb62e9448c174a336d25450c8eb338482e76ba1
Bhushan <bhushanvmulmule@gmail.com>
2021-02-28 13:06:21 +0530

    Moved swagger middleware outside dev env

--- src/Web/Orion.API/Startup.cs
+++ src/Web/Orion.API/Startup.cs
@@ -40,9 +40,10 @@ namespace Orion.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orion.API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orion.API v1"));
 
             app.UseHttpsRedirection();
 
