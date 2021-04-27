11c49fc04458da89564ad8a366278f1d05780e74
Bhushan Mulmule <79776755+bhushan-mulmule@users.noreply.github.com>
2021-04-27 08:55:57 +0530

    prefix 'api' added to the base route (#9)

--- src/Web/Orion.API/Controllers/SeedWork/OrionBaseController.cs
+++ src/Web/Orion.API/Controllers/SeedWork/OrionBaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Orion.API.Controllers.SeedWork
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class OrionBaseController : ControllerBase
     {
         private IMediator _mediator;
