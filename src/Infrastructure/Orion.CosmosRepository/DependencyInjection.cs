efcef9c42ac80b016633d89e0068de91dc017b20
Bhushan <bhushanvmulmule@gmail.com>
2021-05-01 14:14:26 +0530

    Configuration

--- src/Infrastructure/Orion.CosmosRepository/DependencyInjection.cs
+++ src/Infrastructure/Orion.CosmosRepository/DependencyInjection.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Orion.Application.StoryAppLayer.Gateway;
+using Orion.CosmosRepository.Settings;
 using Orion.CosmosRepository.StoryRepositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Orion.CosmosRepository
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddCosmosRepository(this IServiceCollection services)
+        public static IServiceCollection AddCosmosRepository(this IServiceCollection services, IConfiguration configuration)
         {
+            var cosmosSettings = new CosmosSettings();
+            configuration.GetSection(CosmosSettings.SettingName).Bind(cosmosSettings);
+            services.AddSingleton(cosmosSettings);
+
             services.AddSingleton<IStoryCosmosContext, StoryCosmosContext>();
 
             services.AddScoped<IStoryRepository, StoryRepository>();
